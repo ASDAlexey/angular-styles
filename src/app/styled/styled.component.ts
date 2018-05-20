beee20647628d53122e914367a0987fa841d50b3
Alexey <asdalexey@yandex.ru>
2018-05-20 22:24:10 +0300

    add shadow dom styles

--- src/app/styled/styled.component.ts
+++ src/app/styled/styled.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import _ from 'lodash';
 
 @Component({
   selector: 'app-styled',
   templateUrl: './styled.component.html',
-  styleUrls: ['./styled.component.scss']
+  styleUrls: ['./styled.component.scss'],
+  encapsulation: ViewEncapsulation.Native,
 })
 export class StyledComponent implements OnInit {
   data = { a: 'aaa' };
