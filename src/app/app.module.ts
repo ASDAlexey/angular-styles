c6f5bca8a87976270ab7b2f3f7aaaa9d04a760bb
Alexey <asdalexey@yandex.ru>
2018-05-20 22:44:44 +0300

    add basic class switcher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StyledComponent } from './styled/styled.component';
 import { StylishComponent } from './stylish/stylish.component';
+import { CssClassComponent } from './css-class/css-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StyledComponent,
     StylishComponent,
+    CssClassComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/css-class/css-class.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css-class',
+  templateUrl: './css-class.component.html',
+  styleUrls: ['./css-class.component.scss']
+})
+export class CssClassComponent {
+  isBorder = true;
+}
