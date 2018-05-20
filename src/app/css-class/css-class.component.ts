0b8be5894d16ffb7492998409e077dd10580c3af
Alexey <asdalexey@yandex.ru>
2018-05-20 22:54:47 +0300

    add class switcher

--- src/app/css-class/css-class.component.ts
+++ src/app/css-class/css-class.component.ts
@@ -7,4 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CssClassComponent {
   isBorder = true;
+  // cssClasses = 'bold hightlight strike';
+  // cssClasses = ['bold', 'hightlight', 'strike'];
+  // cssClasses = { 'bold': true, 'hightlight': false, 'strike': true };
+
+  isBold = true;
+  isStrike = true;
+  isHightlight = true;
 }
